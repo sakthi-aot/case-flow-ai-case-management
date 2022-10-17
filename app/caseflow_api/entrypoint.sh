e44f79dfa5925c3833a95b3d4098fd67db8bbff9
Akhila <akhila.sathyan@aot-technologies.com>
2022-10-17 17:52:39 +0530

    caseflow api to docker issue

--- app/caseflow_api/entrypoint.sh
+++ app/caseflow_api/entrypoint.sh
@@ -1,4 +1,3 @@
 echo 'starting application'
 export FLASK_APP=manage.py
-flask db upgrade
-gunicorn -b :5000 'caseflow:create_app()' --timeout 120 --worker-class=gthread --workers=1 --threads=2
+gunicorn -b :5000 'caseflow:create_app()' --timeout 120 --worker-class=gthread --workers=1 --threads=1
