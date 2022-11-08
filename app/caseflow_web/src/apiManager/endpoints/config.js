4ca9d29fde0a34250acfe24a9c461fae6045d502
don-aot <don.varghese@aot-technologies.com>
2022-11-08 14:58:52 +0530

    config issue fixing

--- app/caseflow_web/src/apiManager/endpoints/config.js
+++ app/caseflow_web/src/apiManager/endpoints/config.js
@@ -1,7 +1,7 @@
 
 export const CASEFLOW_API_URL =
-(window._env_ && window._env_.REACT_CASEFLOW_API_URL) ||
-process.env.REACT_CASEFLOW_API_URL;
+(window._env_ && window._env_.REACT_APP_CASEFLOW_API_URL) ||
+process.env.REACT_APP_CASEFLOW_API_URL;
 
 export const KEYCLOAK_URL =
 (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL) ||
