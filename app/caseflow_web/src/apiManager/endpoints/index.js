041e3b10303c030fe9ba47f31655f5565fa59a3d
don-aot <don.varghese@aot-technologies.com>
2022-11-08 13:31:49 +0530

    Bugfix

--- app/caseflow_web/src/apiManager/endpoints/index.js
+++ app/caseflow_web/src/apiManager/endpoints/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
- const SERVER_BASE_URL = "http://localhost:5000";
- //import { CASEFLOW_API_URL} from "./config";
+//  const SERVER_BASE_URL = "http://localhost:5000";
+ import { CASEFLOW_API_URL} from "./config";
 
 //alert(CASEFLOW_API_URL)
 const API = {
-  DMS_API: `${SERVER_BASE_URL}/doc`,
+  DMS_API: `${CASEFLOW_API_URL}/doc`,
  
 
 };
