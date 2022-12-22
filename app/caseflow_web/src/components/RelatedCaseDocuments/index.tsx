c773c44e57c925cf535c391fbb06838a5678ed92
GokulVGAot <gokul.vg@aot-technologies.com>
2022-12-22 19:43:51 +0530

    bugfix/ui-changes-case-document:ui channges added

--- app/caseflow_web/src/components/RelatedCaseDocuments/index.tsx
+++ app/caseflow_web/src/components/RelatedCaseDocuments/index.tsx
@@ -8,6 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {useSelector} from "react-redux";
 import { store } from "../../interfaces/stateInterface";
+import "./RelatedCaseDocuments.scss"
 
 
 
@@ -38,16 +39,17 @@ export default function RelatedCaseDocuments() {
     setdocDetail(value)
 
   }, [selectedDocuments]);
-  return (
-    <TableContainer component={Paper} sx={{ boxShadow : 0}}>
-      <Table sx={{ minWidth: 650 ,border : 0}} aria-label="simple table" >
-        <TableHead>
+  return (    
+    <TableContainer component={Paper} sx={{ boxShadow : 0,}}>
+      <Table sx={{ minWidth: 650 ,border : 0,}} aria-label="simple table" className="case-document-table" >
+        <TableHead >
           <TableRow>
-            <TableCell  sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,}} >Name</TableCell>
-            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,}} >Size</TableCell>
-            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,}} >Creation Date</TableCell>
-            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,}} >Last Updated</TableCell>
-            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,}} >version</TableCell>
+            <TableCell  sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} >Name</TableCell>
+            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} >Size</TableCell>
+            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} >Creation Date</TableCell>
+            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} >Last Updated</TableCell>
+            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} >version ##</TableCell>
+            <TableCell align="right" sx={{ color: '#606060',fontWeight: 'bold',fontSize: 16,border:0}} ></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -56,17 +58,23 @@ export default function RelatedCaseDocuments() {
               key={row.name}
               sx={{  border: 0 }}
             >
-              <TableCell style={{borderBottom: "none"}} component="th" scope="row">
-                {row.name}
-              </TableCell>
+              <TableCell style={{borderBottom: "none"}} component="th" scope="row">{row.name}</TableCell>
               <TableCell style={{borderBottom: "none"}} align="right">{row.size ? row.size : "1kb"}</TableCell>
               <TableCell style={{borderBottom: "none"}} align="right">{row.creationdate}</TableCell>
               <TableCell style={{borderBottom: "none"}} align="right">{row.creationdate}</TableCell>
               <TableCell  style={{borderBottom: "none"}} align="right">{row.latestversion}</TableCell>
+              <TableCell  style={{borderBottom: "none"}} align="right">
+                <select className="caseDocumentAction-center" >                
+                <option selected >...</option>
+                <option >Delete</option>
+                <option >Update</option>
+                <option >Merge</option>
+                </select>
+                </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-    </TableContainer>
+    </TableContainer>    
   );
 }
