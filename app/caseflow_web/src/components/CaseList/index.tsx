db4aba77478169865804e988da94c2526a6e94ad
DESKTOP-4SG5713\donba <donbasilpeter@gmail.com>
2022-12-28 11:55:06 +0530

    case search and filter

--- app/caseflow_web/src/components/CaseList/index.tsx
+++ app/caseflow_web/src/components/CaseList/index.tsx
@@ -13,7 +13,6 @@ import { caseListprops,  PropsConfig, RecentCase, SortValue } from "../../interf
 import { getCasesList } from "../../services/CaseService";
 import { setCaseList } from "../../reducers/newCaseReducer";
 import {useSelector,useDispatch} from "react-redux";
-import { store } from "../../interfaces/stateInterface";
 import { Case } from "../../interfaces/componentInterface";
 
 
@@ -28,14 +27,13 @@ import { Case } from "../../interfaces/componentInterface";
 // }
 
 
-const CaseList =React.memo( ({config}:caseListprops) => {
+const CaseList =React.memo( ({config,allRecentCases}:any) => {
 
 
   // const [sortValue,setSortValue] = useState({value:"",sortOrder:null})
   // const [recentCases,setRecentCases] = useState([...allRecentCases])
   // const [sortSelectValue,setSortSelectValues] = useState(sortingkeysOfAllRecentCases)
   const dispatch = useDispatch();
-  let allRecentCases =  useSelector((state:store)=>state.cases.caseList);
 
   // useEffect(()=>{ 
   //  const updatedSortedData = SortCasesByField(sortValue,recentCases)
