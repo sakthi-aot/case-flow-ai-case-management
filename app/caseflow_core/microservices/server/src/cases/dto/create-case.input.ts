0872216f6bc8c9235c5eebacdac1b2c04cc5e5a7
Akhila <akhila.sathyan@aot-technologies.com>
2023-03-17 10:54:05 +0530

    joi validation issue fix

--- app/caseflow_core/microservices/server/src/cases/dto/create-case.input.ts
+++ app/caseflow_core/microservices/server/src/cases/dto/create-case.input.ts
@@ -8,8 +8,6 @@ import { IsArray, IsBoolean, IsDate, IsNotEmpty, IsNumber, IsString } from 'clas
 @InputType()
 export class CreateCaseInput {
   @Field((type) => Int, { nullable: true })
-  @IsNumber()
-  @IsNotEmpty()
   lobid: number;
 
   @Field()
@@ -33,56 +31,36 @@ export class CreateCaseInput {
   typeid: number;
 
   @Field((type) => [Int], { nullable: true })
-  @IsArray()
   linkedcases: number[];
 
   @Field({ defaultValue: new Date() })
-  @IsDate()
-  @IsNotEmpty()
   creationdate: Date;
 
   @Field({ nullable: true })
-  @IsDate()
-  @IsNotEmpty()
   completiondate: Date;
 
   @Field({ nullable: true })
-  @IsDate()
-  @IsNotEmpty()
   lastmodificationdate: Date;
 
   @Field({ nullable: true })
-  @IsDate()
-  @IsNotEmpty()
   penduntildate: Date;
 
   @Field({ nullable: true })
-  @IsDate()
-  @IsNotEmpty()
   archivedate: Date;
 
   @Field((type) => Int, { nullable: true })
-  @IsNumber()
-  @IsNotEmpty()
   startuserid: number;
 
   @Field((type) => Int, { nullable: true })
-  @IsNumber()
-  @IsNotEmpty()
   currentownerid: number;
 
   @Field((type) => [Int], { nullable: true })
-  @IsArray()
   involvedparties: number[];
 
   @Field({ defaultValue: false, nullable: true })
-  @IsBoolean()
-  @IsNotEmpty()
   isdeleted: boolean;
 
   @Field({ nullable: true })
-  @IsNumber()
-  @IsNotEmpty()
   lobcaseid: number;
 
 }
