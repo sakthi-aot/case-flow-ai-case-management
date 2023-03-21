f162c5329adb120b0da13c299b9ddfe2d4ccf692
GokulVGAot <gokul.vg@aot-technologies.com>
2023-03-21 12:33:36 +0530

    feature/nats-server-message-subscribe-page
    ws converted to WSS

--- app/caseflow_web/src/services/NatsServices.ts
+++ app/caseflow_web/src/services/NatsServices.ts
@@ -1,5 +1,5 @@
 import { connect, NatsConnection,Msg   } from 'nats.ws';
-const NATS_URL = 'ws://35.183.76.114:8080';
+const NATS_URL = 'wss://caseflow-natserver.aot-technologies.com';
 
 export const publishMessage = async (SUBJECT,MESSAGE) => {
     try {
